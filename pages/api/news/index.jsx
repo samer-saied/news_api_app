fbf197411e7dcd0fbe0a0723260b58869ab1d6ba
Samer Saied <samerpasha@hotmail.com>
2022-12-26 09:46:10 +0200

    version 1.0.0

--- pages/api/news/index.jsx
+++ pages/api/news/index.jsx
@@ -1,11 +1,26 @@
-import {getNews} from '../../../core/generate'
+import { getNews } from '../../../core/generate'
 
 export default function handler(req, res) {
    if (req.method == "GET") {
       const { pid, country } = req.query
-      getNews(pid, country).then((data) => {
-         res.status(200).json(data)
-      })
+      if (pid && country) {
+         getNews(pid, country).then((data) => {
+            res.status(200).json(data)
+         })
+      } else if (country) {
+         getNews("1", country).then((data) => {
+            res.status(200).json(data)
+         })
+      } else if (pid) {
+         getNews(pid, "arab-and-world/egypt").then((data) => {
+            res.status(200).json(data)
+         })
+      }
+      else {
+         getNews("1", "arab-and-world/egypt").then((data) => {
+            res.status(200).json(data)
+         })
+      }
    } else {
       res.status(400).json({
          message: "Not auth"
