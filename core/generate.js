e79d40a6537ef89b8b0f2788c2ba470d6b298e37
Samer Saied <samerpasha@hotmail.com>
2023-01-17 06:30:51 +0200

    Version 1.1.0

--- core/generate.js
+++ core/generate.js
@@ -88,7 +88,8 @@ export async function getNews(pageNo, countryName) {
             "description": description,
             "image": image,
             "link": newsLink,
-            "topic": topic
+            "topic": topic,
+            "type":"image"
         })
     }
     return {
